0fd83af8f4832dc3f111e4c6a68668668b9984e0
ASHUTOSH DWIVEDI <ashutosh.dwivedi@galgotiacollege.edu>
2020-07-03 19:06:58 +0530

    ADD: Task status and search filter using querybuilder

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -9,7 +9,14 @@ export class TaskRepository extends Repository<Task> {
 
     async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
         const { status, search } = filterDto;
-        const query = this.createQueryBuilder();
+        const query = this.createQueryBuilder('task');
+        if (status) {
+            query.andWhere('task.status=:status', { status });
+
+        }
+        if (search) {
+            query.andWhere('task.title ILIKE :search OR task.description ILIKE :search', { search: `%${search}%` });
+        }
         const tasks = await query.getMany();
         return tasks;
     }
